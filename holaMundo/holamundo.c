7976266b4a9666602b4bb93e9b5c9cab840c0256
goblebunupv <anllosa@upv.es>
2025-10-19 21:01:23 +0200

    Cambios para que funcione el compiladro y la ejecucion con f5

--- holaMundo/holamundo.c
+++ holaMundo/holamundo.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 
 int main() {
     printf("Hola mundo\n");
+    getchar();  // Espera a que el usuario presione una tecla
+
     return 0;
 }
---
+++ p1/p1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+   
+
+
+
+
+float ind_masa_corporal(int peso, int talla) {
+    float imc;
+    imc = (float)peso / ((talla/100.0) * (talla/100.0));
+    printf("Indice de masa corporal: %.2f\n", imc);
+    return imc;
+}
+
+
+
+int main() {
+    setlocale(LC_ALL, "spanish");
+    int peso, talla;
+    
+        // Pedir peso y talla al usuario
+        printf("Introduce el peso en kg: ");
+        scanf("%i", &peso);
+        
+        printf("Introduce la talla en cm: ");
+        scanf("%i", &talla);
+
+
+
+    printf("Peso : %d kg\n", peso);
+    printf("Talla: %d cm\n", talla);
+
+    
+ind_masa_corporal(peso, talla);
+
+
+}
